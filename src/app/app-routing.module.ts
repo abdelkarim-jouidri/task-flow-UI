6289ee6b3c62418a985b975e13a68c1abd550486
abdelkrim-jouidri <abdelkrimjouidri@gmail.com>
2024-01-16 15:11:21 +0100

    added UI for tasks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './shared/home/home.component';
+import { UsersComponent } from './shared/users/users.component';
+import { TasksComponent } from './shared/tasks/tasks.component';
+const routes: Routes = [
+  {path: '', component : HomeComponent},
+  {path : 'users', component : UsersComponent},
+  {path : 'tasks', component : TasksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { HomeComponent } from './shared/home/home.component';
+import { UsersComponent } from './shared/users/users.component';
+import { TasksComponent } from './shared/tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    UsersComponent,
+    TasksComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
